b64b68f3b07cc5443d5b48ed2971a90f10ddf1ad
Alana <alanamuler@hotmail.com>
2025-11-05 13:57:09 +1100

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+// Alana Cristina Muller S1569092
+// Demo project: Managing Java Projects with Git & GitHub
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from version 1!");
+    }
+}
