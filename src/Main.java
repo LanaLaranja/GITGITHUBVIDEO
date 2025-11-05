9ba1b1861191d2c1da97b831222f72dd429b0373
Alana <alanamuler@hotmail.com>
2025-11-05 14:03:07 +1100

    New branch message

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from version 2! - Updated by Alana Muller");
+        System.out.println("This message comes from the new branch");
     }
 }
